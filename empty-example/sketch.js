d6935eea8c9a7a5c7709ef43d3c015e9f2482e1c
phibbert1995 <94574221+phibbert1995@users.noreply.github.com>
2022-04-02 13:41:41 +1100

    Project Setup

---
+++ empty-example/sketch.js
@@ -0,0 +1,16 @@
+//
+//  Project: "Colour My Code"
+//
+//  Assignment 1
+//  ARCH1477 Computational Prototyping for Industrial Design
+//
+//  Patrick Hibbert, s3664204
+//
+
+function setup() {
+  createCanvas(400, 400)
+}
+
+function draw() {
+  background(100)
+}
