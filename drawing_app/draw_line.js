9e82b7e1cc354b62f9d87850e2335fd2784bc9c1
phibbert1995 <94574221+phibbert1995@users.noreply.github.com>
2022-04-03 09:44:54 +1000

    changes to export function

--- drawing_app/draw_line.js
+++ drawing_app/draw_line.js
@@ -1,7 +1,7 @@
 //  sketch for drawing line
 
 class draw_line {
-    constructor(pencilColour, pencilWidth) {
+    constructor(pencilColour, pencilWidth, pencilShape) {
         this.px = pwinMouseX
         this.py = pwinMouseY
         this.x = winMouseX
@@ -9,10 +9,59 @@ class draw_line {
 
         this.pencilColour = pencilColour
         this.pencilWidth = pencilWidth
+        this.pencilShape = pencilShape
     }
     show() {
-        stroke(this.pencilColour)
-        strokeWeight(this.pencilWidth)
-        line(this.px, this.py, this.x, this.y)
+        if (this.pencilShape === 'Line') {
+            stroke(this.pencilColour)
+             strokeWeight(this.pencilWidth)
+             line(this.px, this.py, this.x, this.y)
+        }
+        if (this.pencilShape === 'Circle') {
+            fill(this.pencilColour)
+            noStroke()
+            ellipse(this.x, this.y, this.pencilWidth)
+        }
+        if (this.pencilShape === 'Rectangle') {
+            fill(this.pencilColour)
+            noStroke()
+            rect(this.x, this.y, this.pencilWidth)
+        }
+        if (this.pencilShape === 'Triangle') {
+            fill(this.pencilColour)
+            noStroke()
+            
+            push()
+
+            translate(this.x, this.y)
+
+            beginShape()
+            for (var i = 0; i < 360; i += 120) {
+                var rad = this.pencilWidth / 2
+                var x = rad * cos(i)
+                var y = rad * sin(i)
+                vertex(x, y)
+            }
+            endShape(CLOSE)
+            pop()
+        }
+        if (this.pencilShape === 'Star') {
+            fill(this.pencilColour)
+            noStroke()
+            
+            push()
+
+            translate(this.x, this.y)
+
+            beginShape()
+            for (var i = 0; i < 720; i += 720 / 5) {
+                var rad = this.pencilWidth / 2
+                var x = rad * cos(i)
+                var y = rad * sin(i)
+                vertex(x, y)
+            }
+            endShape(CLOSE)
+            pop()
+        }
     }
 }
--- drawing_app/drawing_app_sketch.js
+++ drawing_app/drawing_app_sketch.js
@@ -12,43 +12,72 @@ var pencilColour
 var backgroundColour
 var pencilWidth
 var clearCanvas
+var pencilShape
+var pencilRandom
 
 function setup() {
-  createCanvas(400, 450)
+  let c = createCanvas(windowWidth, windowHeight - 250)
+  angleMode(DEGREES)
 
   var options = createDiv().style('display: flex')
 
   var optionsTitles = createDiv().parent(options)
-  createP('Pencil Colour').parent(optionsTitles)
   createP('Background Colour').parent(optionsTitles)
+  createP('Pencil Colour').parent(optionsTitles)
   createP('Pencil Width').parent(optionsTitles)
+  createP('Pencil Shape').parent(optionsTitles)
 
-  var optionsValues = createDiv().parent(options).style('margin: 10px 40px; width: 50px')
-  pencilColour = createColorPicker('#ffffff').parent(optionsValues)
+  var optionsValues = createDiv().parent(options).style('margin: 10px 40px; width: 80px')
   backgroundColour = createColorPicker('#1e1e1e').parent(optionsValues).style('margin-top: 10px')
-  pencilWidth = createSelect(false).parent(optionsValues).style('margin-top: 10px')
-  pencilWidth.option('1')
+  pencilColour = createColorPicker('#ffffff').parent(optionsValues)
+  pencilRandom = createCheckbox("", false).parent(optionsValues).style('display: inline')
+  pencilWidth = createSelect(false).parent(optionsValues).style('margin-top: 10px; width: 50px; height: 25px')
   pencilWidth.option('2')
-  pencilWidth.option('3')
   pencilWidth.option('4')
-  pencilWidth.selected('2')
+  pencilWidth.option('8')
+  pencilWidth.option('16')
+  pencilWidth.option('32')
 
-  clearCanvas = createButton('Clear').parent(options).style('width: 100px')
+  pencilShape = createSelect(false).parent(optionsValues).style('margin-top: 10px; width: 50px; height: 25px')
+  pencilShape.option('Line')
+  pencilShape.option('Circle')
+  pencilShape.option('Rectangle')
+  pencilShape.option('Triangle')
+  pencilShape.option('Star')
+
+  clearCanvas = createButton('Clear').parent(options).style('margin: 10px; width: 100px; height: 100px')
+
+  saveCanvas = createButton("Save");
 }
 
 function draw() {
   background(backgroundColour.value())
+
+  if (pencilRandom.checked()) {
+   var r = hex(floor(map(noise(frameCount / 100), 0, 1, 0, 255)), 2)
+   var g = hex(floor(map(noise(frameCount / 100 + 1000), 0, 1, 0, 255)), 2)
+   var b = hex(floor(map(noise(frameCount / 100 + 2000), 0, 1, 0, 255)), 2)
+
+   pencilColour.value('#' + r + g + b)
+  }
 
   clearCanvas.mousePressed(function() {
     lines = []
   })
 
   if (mouseIsPressed) {
-      var line = new draw_line(pencilColour.value(), pencilWidth.value())
+      var line = new draw_line(pencilColour.value(), pencilWidth.value(), pencilShape.value())
       lines.push(line)
   }
 
   for (var line of lines) {
       line.show()
   }
+
+  saveCanvas.mousePressed(saveAsCanvas)
 }
+
+function saveAsCanvas() {
+  save("my_masterpiece.png");
+}
+
